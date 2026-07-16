b961267df51be6e273b8599d447fde08d86492a9
mediinfo2026 <mediinfo2026@gmail.com>
2026-07-16 22:23:14 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAW6fOFxcKNtS7iB4XpGEHyoQKKbL_yXSA",
+  authDomain: "mediinfo-bd.firebaseapp.com",
+  projectId: "mediinfo-bd",
+  storageBucket: "mediinfo-bd.firebasestorage.app",
+  messagingSenderId: "903946041697",
+  appId: "1:903946041697:web:4bc1fe6e053896a282ed20",
+  measurementId: "G-RYBYTCY1V4"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
