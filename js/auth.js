e1049447fda8848f63b5f1ef587b9add9b38776e
mediinfo2026 <mediinfo2026@gmail.com>
2026-07-17 21:26:16 +0600

    Implement authentication functions in auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,210 @@
+// js/auth.js
 
+
+import { auth } from "../firebase.js";
+
+
+import {
+
+onAuthStateChanged,
+signOut
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+
+
+
+
+// Current User Check
+
+
+export function checkAuth(callback){
+
+
+
+onAuthStateChanged(
+
+auth,
+
+(user)=>{
+
+
+if(callback){
+
+
+callback(user);
+
+
+}
+
+
+
+}
+
+
+);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// Require Admin Login
+
+
+export function requireAuth(){
+
+
+
+onAuthStateChanged(
+
+auth,
+
+(user)=>{
+
+
+
+if(!user){
+
+
+window.location.href =
+"../admin/login.html";
+
+
+}
+
+
+
+}
+
+
+);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// Get Current User
+
+
+export function getCurrentUser(){
+
+
+
+return auth.currentUser;
+
+
+}
+
+
+
+
+
+
+
+
+
+// Logout Function
+
+
+export async function logout(){
+
+
+
+try{
+
+
+await signOut(auth);
+
+
+
+window.location.href =
+"../admin/login.html";
+
+
+
+}
+
+
+catch(error){
+
+
+console.log(
+"Logout Error:",
+error
+);
+
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// Show User Email
+
+
+export function showUserEmail(elementId){
+
+
+
+const user =
+
+auth.currentUser;
+
+
+
+const element =
+
+document.getElementById(
+elementId
+);
+
+
+
+
+
+if(
+element &&
+user
+){
+
+
+element.innerHTML =
+user.email;
+
+
+}
+
+
+
+}
